a3c1fda56eb348fa9b57e2aa818c44ed905be560
TOLINSKI <ben.tulchinski@gmail.com>
2024-05-26 13:59:57 +0100

    First commit project files

---
+++ LastStandAwakening/Source/LastStandAwakening/LastStandAwakening.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LastStandAwakening.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LastStandAwakening, "LastStandAwakening" );
